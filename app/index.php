4f543613ca81dd736958b77e1389df62e9d906c8
PedroPereira78 <you@example.com>
2025-11-22 19:30:27 +0000

    V1

---
+++ app/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Página inicial
+if ($_SERVER["REQUEST_URI"] === "/" || $_SERVER["REQUEST_URI"] === "/index.php") {
+    readfile("home.html");
+    exit;
+}
+
+header("Content-Type: application/json");
+
+echo json_encode([
+    "mensagem" => "Bem-vindo ao Activity Provider!",
+    "autor" => "Pedro Pereira"
+], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
